9d644981381673544ab4209651926f900191e0fd
PushpaKawadi <pushpa.kawadi@thoughtfocus.com>
2025-11-17 10:00:22 -0800

    Commit config changes

--- core/src/main/java/com/csuf/cloud/core/servlets/CloudSampleServlet.java
+++ core/src/main/java/com/csuf/cloud/core/servlets/CloudSampleServlet.java
@@ -25,8 +25,8 @@ public class CloudSampleServlet extends SlingSafeMethodsServlet {
 	@Reference
 	transient private GlobalConfigCSUFService globalConfigFilenetService;
 	
-	/*@Reference
-	transient private BulkApprovalFactoryConfigService bulkApproval;*/
+	@Reference
+	transient private BulkApprovalFactoryConfigService bulkApproval;
 
 	@Override
 	protected void doGet(SlingHttpServletRequest request, SlingHttpServletResponse response) throws IOException {
@@ -41,16 +41,16 @@ public class CloudSampleServlet extends SlingSafeMethodsServlet {
 		response.getWriter().write(jsonResponse);
         response.getWriter().write(jsonResponse1);
         
-        /*String uID = bulkApproval.uniqueIdentifier();
+        String uID = bulkApproval.uniqueIdentifier();
 		String uIDRes = "{\"uID\":\"" + uID + "\"}";
 		
-		 List<String> xmlVal = bulkApproval.xmlFieldsToUpdate();
+        /*List<String> xmlVal = bulkApproval.xmlFieldsToUpdate();
 		 xmlVal.get(1);
 		 xmlVal.get(1);
 		
 		 response.getWriter().write(xmlVal.get(1));
-		 response.getWriter().write(xmlVal.get(2));
-		 response.getWriter().write(uIDRes);*/
+		 response.getWriter().write(xmlVal.get(2));*/
+		 response.getWriter().write(uIDRes);
 		
 		
 	}
