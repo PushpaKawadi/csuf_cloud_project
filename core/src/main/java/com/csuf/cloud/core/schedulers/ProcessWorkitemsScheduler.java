3548848091bf30b48d8b39c99377b0968c83622c
PushpaKawadi <pushpa.kawadi@thoughtfocus.com>
2025-12-15 11:57:49 -0800

    Commit

--- core/src/main/java/com/csuf/cloud/core/schedulers/ProcessWorkitemsScheduler.java
+++ core/src/main/java/com/csuf/cloud/core/schedulers/ProcessWorkitemsScheduler.java
@@ -121,7 +121,7 @@ public class ProcessWorkitemsScheduler implements Runnable {
 	 */
 	@Override
 	public void run() {
-		log.info("Irvine starting Process Active Workitems on Processing Instance Scheduler Service...");
+		log.info("Pushpa starting Process Active Workitems on Processing Instance Scheduler Service...");
 		WorkflowSession wfSession = null;
 		ResourceResolver resolver = null;
 		Session session = null;
