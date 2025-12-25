1170bb6576670d9522e69a4849b75b70190c55c1
PushpaKawadi <pushpa.kawadi@thoughtfocus.com>
2025-12-24 16:32:13 -0800

    Commit

--- core/src/main/java/com/csuf/cloud/core/services/impl/TaskServiceImpl.java
+++ core/src/main/java/com/csuf/cloud/core/services/impl/TaskServiceImpl.java
@@ -1091,12 +1091,12 @@ public class TaskServiceImpl implements TaskService {
 				}*/
 				JsonObject jsonObj = new JsonObject();
 				
-				jsonObj.addProperty("isViewTaskAllowed", false);
+				jsonObj.addProperty("isViewTaskAllowed", true);
 				jsonObj.addProperty("isAssigneeAGroup", false);
 				
 				
-				jsonObj.addProperty("isViewTaskDetailsAllowed", false);
-				jsonObj.addProperty("isCurrentUserAdmin", "admin");
+				jsonObj.addProperty("isViewTaskDetailsAllowed", true);
+				jsonObj.addProperty("isCurrentUserAdmin", false);
 				jsonObj.addProperty("currentUserId", "yjayaram@fullerton.edu");
 
 				// ----- AEM Permission Block -----
