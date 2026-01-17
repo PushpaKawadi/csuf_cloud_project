c25d2ae06f97fe70e41a03f061ae6360ae808eda
PushpaKawadi <pushpa.kawadi@thoughtfocus.com>
2026-01-16 19:12:27 -0800

    Commit

--- core/src/main/java/com/csuf/cloud/core/services/impl/InboxItemServiceImpl.java
+++ core/src/main/java/com/csuf/cloud/core/services/impl/InboxItemServiceImpl.java
@@ -253,14 +253,15 @@ public class InboxItemServiceImpl implements InboxItemService {
 			} else {
 				throw new Exception("payload path is empty inside getTaskAttachments method");
 			}
-			log.info("LosAngles ITR = {}", itr.hashCode());
+			/*log.info("LosAngles ITR = {}", itr.hashCode());
 			log.info("LosAngles ITR = {}", itr.hasNext());
 			log.info("LosAngles ITR = {}", itr.toString());
-			log.info("LosAngles ITR = {}", itr.getClass());
+			log.info("LosAngles ITR = {}", itr.getClass());*/
+			log.info("LosAngles ITR = {}", itr.next());
 
 			
 			
-			while (itr.hasNext()) {
+			//while (itr.hasNext()) {
 				log.info("inside itr");
 				Node node = itr.next();
 				String path = node.getPath();
@@ -275,7 +276,7 @@ public class InboxItemServiceImpl implements InboxItemService {
 						formsJson.add(json);
 					}
 				}
-			}
+			//}
 		}
 		return formsJson;
 	}
